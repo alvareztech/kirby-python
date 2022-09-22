3a7d1c1b4881c584d5aeb2d2a70563b86cbc7cfa
Daniel Alvarez <daniel@alvarez.tech>
2022-09-22 10:13:05 -0700

    gh updates

--- main.py
+++ main.py
@@ -15,34 +15,40 @@ def checkFloat(number):
     except ValueError:
         return None
 
-
-cred = credentials.Certificate("firebase.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-rates = db.collection('rates')
-
-URL = "https://www.bcb.gob.bo/librerias/indicadores/otras/ultimo.php"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-table = soup.find("table", class_="tablaborde")
-# print(table.prettify())
-rows = table.find_all("tr")
-
-for i, row in enumerate(rows):
-    if i == 0:
-        continue
-    cols = row.find_all('td')
-    country = cols[0].text.strip()
-    name = cols[1].text.strip()
-    currency = cols[2].text.strip()
-    exchange = cols[3].text.strip()
-    exchangeME = cols[4].text.strip()
-    print("%s %s %s %s %s" % (country, name, currency, exchange, exchangeME))
-    rates.add({'country': country, 'name': name, 'currency': currency, 'exchange': checkFloat(exchange),
-              'exchangeME': checkFloat(exchangeME), 'source': 'BO', 'date': SERVER_TIMESTAMP})
-
-
 # print(results.prettify())
+
+
+def main():
+    print("Hello World!")
+    cred = credentials.Certificate("firebase.json")
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+    rates = db.collection('rates')
+
+    URL = "https://www.bcb.gob.bo/librerias/indicadores/otras/ultimo.php"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    table = soup.find("table", class_="tablaborde")
+    # print(table.prettify())
+    rows = table.find_all("tr")
+
+    for i, row in enumerate(rows):
+        if i == 0:
+            continue
+        cols = row.find_all('td')
+        country = cols[0].text.strip()
+        name = cols[1].text.strip()
+        currency = cols[2].text.strip()
+        exchange = cols[3].text.strip()
+        exchangeME = cols[4].text.strip()
+        print("%s %s %s %s %s" %
+              (country, name, currency, exchange, exchangeME))
+        rates.add({'country': country, 'name': name, 'currency': currency, 'exchange': checkFloat(exchange),
+                   'exchangeME': checkFloat(exchangeME), 'source': 'BO', 'date': SERVER_TIMESTAMP})
+
+
+if __name__ == "__main__":
+    main()
