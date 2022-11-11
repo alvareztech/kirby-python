596b500af9c47f1d40dc9bb91dc47dc659d7374e
Daniel Alvarez <daniel@alvarez.tech>
2022-11-11 11:18:20 -0800

    Price Date parameter added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import date
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import locale
 
 import datetime
 from firebase_admin.firestore import SERVER_TIMESTAMP
@@ -18,21 +19,31 @@ def checkFloat(number):
 # print(results.prettify())
 
 
+BO_BCB_URL = "https://www.bcb.gob.bo/librerias/indicadores/otras/ultimo.php"
+
+
 def main():
-    print("Hello World!")
+    locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+
     cred = credentials.Certificate("firebase.json")
     firebase_admin.initialize_app(cred)
 
     db = firestore.client()
-    rates = db.collection('rates')
+    ratesCollection = db.collection('rates')
 
-    URL = "https://www.bcb.gob.bo/librerias/indicadores/otras/ultimo.php"
-    page = requests.get(URL)
+    page = requests.get(BO_BCB_URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
+    priceDateRaw = soup.select_one(
+        "table").select_one("td").select_one("strong")
+    print(priceDateRaw.string)
+    priceDate = datetime.datetime.strptime(
+        priceDateRaw.string, "%d de %B %Y").date()
+    priceDate = priceDate.strftime("%d-%m-%Y")
+    print(priceDate)
+
     table = soup.find("table", class_="tablaborde")
-    # print(table.prettify())
     rows = table.find_all("tr")
 
     for i, row in enumerate(rows):
@@ -46,8 +57,8 @@ def main():
         exchangeME = cols[4].text.strip()
         print("%s %s %s %s %s" %
               (country, name, currency, exchange, exchangeME))
-        rates.add({'country': country, 'name': name, 'currency': currency, 'exchange': checkFloat(exchange),
-                   'exchangeME': checkFloat(exchangeME), 'source': 'BO', 'date': SERVER_TIMESTAMP})
+        ratesCollection.add({'country': country, 'name': name, 'currency': currency, 'exchange': checkFloat(exchange),
+                             'exchangeME': checkFloat(exchangeME), 'source': 'BO', 'priceDate': priceDate, 'date': SERVER_TIMESTAMP})
 
 
 if __name__ == "__main__":
